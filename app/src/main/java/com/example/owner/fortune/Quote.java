7f75def54d9cf075e1fb6ad21fe67dbb6b444195
Jun Trinh <dungtrinh1993@gmail.com>
2016-07-30 22:39:11 -0700

    - Add the new items in menu bar - Finish onPause/onStop function using Save/RestoreInstance state - Update some documentations - Change Quote to implements Parcelable class

--- app/src/main/java/com/example/owner/fortune/Quote.java
+++ app/src/main/java/com/example/owner/fortune/Quote.java
@@ -1,11 +1,14 @@
 package com.example.owner.fortune;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 import java.util.*;
 
 /**
  * Created by Dung Trinh on 6/20/2016.
  */
-public class Quote {
+public class Quote implements Parcelable {
     /** The main content of the quote*/
     private String content;
 
@@ -13,6 +16,19 @@ public class Quote {
     {
         this.content = content;
     }
+
+    public static final Creator<Quote> CREATOR = new Creator<Quote>() {
+        @Override
+        public Quote createFromParcel(Parcel in) {
+            Quote joke = new Quote(in.readString());
+            return joke;
+        }
+
+        @Override
+        public Quote[] newArray(int size) {
+            return new Quote[size];
+        }
+    };
 
     public String toString()
     {
@@ -28,4 +44,13 @@ public class Quote {
         return true;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(content);
+    }
 }
